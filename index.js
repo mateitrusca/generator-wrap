8a5630e611d19e7b0103a23eaeab0d174352e0e2
Adrian Punga <adrian.punga@kalon.ro>
2014-12-08 10:23:40 +0200

    Simplified further as Promise.resolve does auto wrapping into Promise, too.

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ module.exports = function wrap(genFunc) {
 	return function () {
 		let gen = genFunc.apply(this, arguments);
 
-		function toPromise(x) {
-			if (x !== undefined && x !== null && typeof x.then === 'function') {
-				return x;
-			} else {
-				return Promise.resolve(x);
-			}
-		}
-
 		function next(err, result) {
 			let generatorResult = undefined;
 			if (err !== undefined) {
@@ -19,7 +11,7 @@ module.exports = function wrap(genFunc) {
 				generatorResult = gen.next(result);
 			}
 			if (generatorResult.done === false) {
-				toPromise(generatorResult.value)
+				Promise.resolve(generatorResult.value)
 					.then(
 					function (result) {
 						setImmediate(function () {
